912ae4c7de71aec58a9fdc05f2e3ddb425f44c2b
RNrd00 <117254446+RNrd00@users.noreply.github.com>
2022-12-19 08:41:19 +0000

    [Finish]Application-task4

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users
   root to: 'homes#top'
   get "home/about" => "homes#about"
+
+  devise_for :users
+
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy, :followings, :followers]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 end
