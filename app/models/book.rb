0ce2e0cf860740bf0e2087ad574eaa271611b05d
RNrd00 <117254446+RNrd00@users.noreply.github.com>
2022-12-20 00:40:52 +0000

    [Finish]Application-task5

--- app/models/book.rb
+++ app/models/book.rb
@@ -12,6 +12,20 @@ class Book < ApplicationRecord
   
   has_one_attached :profile_image
   
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @book = Book.where("title LIKE?","#{word}")
+    elsif search == "forward_match"
+      @book = Book.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @book = Book.where("title LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @book = Book.where("title LIKE?","%#{word}%")
+    else
+      @book = Book.all
+    end
+  end
+  
 def get_profile_image(width, height)
   unless profile_image.attached?
     file_path = Rails.root.join('app/assets/images/no_image.jpg')
